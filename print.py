2a70c52a5dcc5b3209d04dde7ebf5cbaa06bbe30
ZhiyuanYu133 <77706312+ZhiyuanYu133@users.noreply.github.com>
2023-01-13 16:19:34 -0700

    Update print.py

--- print.py
+++ print.py
@@ -1,2 +1,6 @@
 import requests
 print(requests.__version__)
+response = requests.get("http://google.com")
+print(response.content)
+get_script = requests.get("https://raw.githubusercontent.com/ZhiyuanYu133/Cmput404/main/print.py")
+print(get_script.text)
